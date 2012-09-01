5cc39d106a92b876c2bf8b001fbf4ffb54a01ce0
Ｖ字龍(Vdragon) <pika1021@gmail.com>
2012-09-01 23:46:09 +0800

    第一次提交變更，加入檔案

---
+++ Executables/template.sh
@@ -0,0 +1,23 @@
+﻿#!/bin/bash
+# 上一句用來宣告執行script用的shell
+####################################################
+# Script程式名稱 | Script Name
+# 	
+# 著作權宣告 | Copyright Declaration
+# 	Ｖ字龍(Vdragon) (c) 2012
+# 授權條款宣告 | License Declaration
+# 	GNU Lesser General Public License version 3.0 or later
+# 	http://www.gnu.org/licenses/lgpl.html
+# 傳回值定義 | Return Value Definition
+# 	0
+# 		正常結束
+# 已知問題 | Known Issues
+# 	Known issues is now on GitHub
+# 	
+# 修訂紀錄 | Changelog
+# 	Changelog is now on GitHub
+# 	
+###################################################
+
+# 正常結束script程式
+exit 0
---
+++ README.txt
@@ -0,0 +1,6 @@
+新殼程式(shell) script程式範本
+New shell script template
+--------------------------------------
+授權條款宣告 | License Declaration
+  GNU Lesser General Public License version 3.0 or later
+  http://www.gnu.org/licenses/lgpl.html
