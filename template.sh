5b8d795e1b01a4e992c2e111444f5258298053c7
Ｖ字龍(Vdragon) <pika1021@gmail.com>
2012-12-01 15:53:20 +0800

    Indentation fixes and BOM removal

--- template.sh
+++ template.sh
@@ -1,21 +1,21 @@
-﻿#!/bin/bash
-# 上一句用來宣告執行script用的shell
+#!/bin/bash
+# 		宣告執行script用的shell
 ####################################################
 # Script程式名稱 | Script Name
-# 	
+# 		template.sh
 # 著作權宣告 | Copyright Declaration
-# 	Ｖ字龍(Vdragon) (c) 2012
+# 		Ｖ字龍(Vdragon) <pika1021@gmail.com> (c) 2012
 # 授權條款宣告 | License Declaration
-# 	GNU Lesser General Public License version 3.0 or later
-# 	http://www.gnu.org/licenses/lgpl.html
+# 		GNU Lesser General Public License version 3.0 or later
+# 			http://www.gnu.org/licenses/lgpl.html
 # 傳回值定義 | Return Value Definition
-# 	0
-# 		正常結束
+# 		0
+# 			正常結束
 # 已知問題 | Known Issues
-# 	Known issues is now on GitHub
+# 		Known issues is now on GitHub
 # 	
 # 修訂紀錄 | Changelog
-# 	Changelog is now on GitHub
+# 		Changelog is now on GitHub
 # 	
 ###################################################
 
