3789268d51b0d3b465c2468adf89b910a95e55e3
Ｖ字龍(Vdragon) <pika1021@gmail.com>
2012-12-01 16:17:28 +0800

    Add source script template

---
+++ Template.source.sh
@@ -0,0 +1,15 @@
+####################################################
+# Script 名稱 | Script Name
+# 		template.source.sh
+# 著作權宣告 | Copyright Declaration
+# 		Ｖ字龍(Vdragon) <pika1021@gmail.com> (c) 2012
+# 授權條款宣告 | License Declaration
+# 		GNU Lesser General Public License version 3.0 or later
+# 			http://www.gnu.org/licenses/lgpl.html
+# 已知問題 | Known Issues
+# 		Known issues is now on GitHub
+# 	
+# 修訂紀錄 | Changelog
+# 		Changelog is now on GitHub
+# 	
+###################################################
