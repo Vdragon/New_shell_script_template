5113a1ea3ba7c59bdf9ee80527f3b514809aeece
Ｖ字龍(Vdragon) <pika1021@gmail.com>
2013-10-09 02:43:33 +0800

    將 header 中項目名稱改成中英雙行

--- Template.source.sh
+++ Template.source.sh
@@ -1,16 +1,25 @@
 ################## Header block ##################
-# Script 程式名稱 | Script Name
-# 		template.sh
-# 著作權宣告 | Copyright Declaration
-# 		Ｖ字龍(Vdragon) <pika1021@gmail.com> (c) 2012
-# 授權條款宣告 | License Declaration
-# 		GNU Lesser General Public License version 3.0 or later
-# 			http://www.gnu.org/licenses/lgpl.html
-# 已知問題 | Known Issues
-# 		Known issues is now on GitHub
+# Script程式名稱
+# Script Name
 # 	
-# 修訂紀錄 | Changelog
-# 		Changelog is now on GitHub
+# 著作權宣告
+# Copyright Declaration
+# 	Ｖ字龍(Vdragon) <pika1021@gmail.com> (c) 2012
+# 授權條款宣告
+# License Declaration
+# 	GNU Lesser General Public License version 3.0 or later
+# 	http://www.gnu.org/licenses/lgpl.html
+# 傳回值定義
+# Return Value Definition
+# 	0
+# 		正常結束
+# 已知問題
+# Known Issues
+# 	Known issues is now tracked on GitHub
+# 	
+# 修訂紀錄
+# Changelog
+# 	Changelog is now tracked using Git repostiory commit log
 # 	
 ############## Header block ended ##############
 
