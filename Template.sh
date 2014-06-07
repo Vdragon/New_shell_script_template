b17a654ba4381d233659d716f95382c0a24347b3
Ｖ字龍(Vdragon) <pika1021@gmail.com>
2014-06-07 17:31:28 +0800

    Script - 將 PROGRAM_ARGUMENT_LIST 全域變數重新命名為 PROGRAM_ARGUMENT_ORIGINAL_LIST
    Signed-off-by: Ｖ字龍(Vdragon) <pika1021@gmail.com>

--- Template.sh
+++ Template.sh
@@ -33,7 +33,7 @@
 # idea from http://www.kfirlavi.com/blog/2012/11/14/defensive-bash-programming/
 readonly PROGRAM_NAME="$(basename $0)"
 readonly PROGRAM_DIRECTORY="$(readlink -m $(dirname $0))"
-readonly PROGRAM_ARGUMENT_LIST="$@"
+readonly PROGRAM_ARGUMENT_ORIGINAL_LIST="$@"
 readonly PROGRAM_ARGUMENT_ORIGINAL_NUMBER=$#
 
 ######## File scope variable definitions ended ########
