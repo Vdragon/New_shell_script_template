81d00ad3e37290d04867a382777d1fa867af0eb7
Ｖ字龍(Vdragon) <pika1021@gmail.com>
2014-06-18 06:58:13 +0800

    Template - 美觀修正
    Signed-off-by: Ｖ字龍(Vdragon) <pika1021@gmail.com>

--- Template.sh
+++ Template.sh
@@ -39,8 +39,6 @@ readonly PROGRAM_ARGUMENT_ORIGINAL_NUMBER=$#
 ######## File scope variable definitions ended ########
 
 ######## Program ########
-
-
 # main function, program entry point
 # idea from http://www.kfirlavi.com/blog/2012/11/14/defensive-bash-programming/
 main() {
